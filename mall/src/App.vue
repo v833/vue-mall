df66fbbaa4f6caddc1dc3096beec82d45bbae5ef
v833 <2507301541@qq.com>
2021-06-04 14:32:17 +0800

    mock

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -5,15 +5,23 @@
 </template>
 
 <script>
-import storage from './storage/index'
 export default {
+  name: 'app',
   data () {
     return {
-
+      res: {}
     }
   },
   mounted() {
-    
+    // 本地加载请求静态json
+    // this.axios.get('/mock/user/login.json').then(res => {
+    //   this.res = res
+    // })
+
+    // 本地集成mockjs实现数据mock 没有发送真正的请求，代码阶段拦截
+    this.axios.get('/user/login').then(res => {
+      this.res = res
+    })
   },
 }
 </script>
--- mall/src/main.js
+++ mall/src/main.js
@@ -6,12 +6,18 @@ import axios from 'axios' // 每个页面都要去写，导入
 import VueAxios from 'vue-axios' // 作用域对象挂载在vue实例上
 import env from './env'
 
+const mock = true; // 希望mock开关打开时被拦截，用require而不是import
+if (mock) {
+  // import 预编译加载 require 执行加载，如果未需要，不加载
+  require('./mock/api')
+}
+
 // 根据前端的跨域方式做调整 (接口代理除外)
 axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000; // 超时时间
-// 根据环境变量获取不同请求的地址
 
-axios.defaults.baseURL = env.baseURL
+// 根据环境变量获取不同请求的地址
+// axios.defaults.baseURL = env.baseURL
 // 接口错误拦截
 axios.interceptors.response.use(function (response) {
   let res = response.data;
