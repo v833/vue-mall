a5160939739677c43c86bbd48aef3293b2499f7d
v833 <2507301541@qq.com>
2021-06-04 21:04:55 +0800

    nav-header

--- mall/src/components/NavHeader.vue
+++ mall/src/components/NavHeader.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    
+  <div class="header">
+    <div class="nav-topbar">
+      <div class="container">
+        <div class="topbar-menu">
+          <a href="javascript:;">小米商城</a>
+          <a href="javascript:;">MUI</a>
+          <a href="javascript:;">云服务</a>
+          <a href="javascript:;">协议规则</a>
+        </div>
+        <div class="topbar-user">
+          <a href="javascript:;">登录</a>
+          <a href="javascript:;">注册</a>
+          <a href="javascript:;">购物车</a>
+        </div>
+      </div>
+    </div>
+    <div class="nav-header"></div>
   </div>
 </template>
 
@@ -10,6 +25,10 @@ export default {
 }
 </script>
 
-<style lang="">
-  
+<style lang="scss" scoped>
+  .header {
+    .container {
+      width: 1226px;
+    }
+  }
 </style>
