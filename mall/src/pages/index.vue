185d3099de5e91577d62296a1cce298490dac667
v833 <2507301541@qq.com>
2021-06-05 16:14:35 +0800

    首页轮播

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -1,17 +1,65 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item, index) in sliderList" :key="index">
+            <a href="'/#/product/' + item.id"><img :src="item.img"></a>
+          </swiper-slide>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
 import ServiceBar from '../components/ServiceBar.vue'
+import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 export default {
-  components: { ServiceBar },
-  name: 'index'
+  name: 'index',
+  components: { 
+    ServiceBar,
+    Swiper,
+    SwiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+
+      },
+      sliderList: [
+        {
+          id: '42',
+          img: '/imgs/slider/slider-1.jpg'
+        },
+        {
+          id: '45',
+          img: '/imgs/slider/slider-2.jpg'
+        },
+        {
+          id: '46',
+          img: '/imgs/slider/slider-3.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slider-4.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slider-5.jpg'
+        },
+      ]
+    }
+  }
+  
 }
 </script>
 
-<style lang="">
+<style lang="scss">
   
 </style>
