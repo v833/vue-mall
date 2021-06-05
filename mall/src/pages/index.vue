6eac667f2b039111ebfc8bb7d5104419c6d152ec
v833 <2507301541@qq.com>
2021-06-05 16:56:08 +0800

    swiper

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -3,9 +3,13 @@
     <div class="container">
       <div class="swiper-box">
         <swiper :options="swiperOption">
-          <swiper-slide v-for="(item, index) in sliderList" :key="index">
-            <a href="'/#/product/' + item.id"><img :src="item.img"></a>
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a :href="'/#/product/' + item.id"><img :src="item.img"></a>
           </swiper-slide>
+          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+          <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
         </swiper>
       </div>
       <div class="ads-box"></div>
@@ -18,40 +22,53 @@
 
 <script>
 import ServiceBar from '../components/ServiceBar.vue'
-import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import { swiper, swiperSlide} from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 export default {
   name: 'index',
   components: { 
     ServiceBar,
-    Swiper,
-    SwiperSlide
+    swiper,
+    swiperSlide
   },
   data () {
     return {
       swiperOption: {
-
+        autoplay: true,
+        // 循环播放
+        loop: true,
+        // 切换效果
+        effect: 'cube',
+        pagination: {
+          el: '.swiper-pagination',
+          // 点击分页器切换
+          clickable :true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
       },
-      sliderList: [
+      slideList: [
         {
           id: '42',
-          img: '/imgs/slider/slider-1.jpg'
+          img: '/imgs/slider/slide-1.jpg'
         },
         {
           id: '45',
-          img: '/imgs/slider/slider-2.jpg'
+          img: '/imgs/slider/slide-2.jpg'
         },
         {
           id: '46',
-          img: '/imgs/slider/slider-3.jpg'
+          img: '/imgs/slider/slide-3.jpg'
         },
         {
           id: '',
-          img: '/imgs/slider/slider-4.jpg'
+          img: '/imgs/slider/slide-4.jpg'
         },
         {
           id: '',
-          img: '/imgs/slider/slider-5.jpg'
+          img: '/imgs/slider/slide-5.jpg'
         },
       ]
     }
@@ -61,5 +78,14 @@ export default {
 </script>
 
 <style lang="scss">
-  
+  .index {
+    .swiper-box {
+      .swiper-container {
+        height: 451px;
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
 </style>
