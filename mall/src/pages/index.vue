8d51f36b062b0a5a47690541930f3d287953d2a2
v833 <2507301541@qq.com>
2021-06-05 16:30:43 +0800

    update

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
+    <swiper :options="swiperOption">
+      <swiper-slide></swiper-slide>
+    </swiper>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import { swiper, swiperSlide} from 'vue-awesome-swiper' // swiper组件很大，按需加载
+import 'swiper/dist/css/swiper.css'
 import ServiceBar from '../components/ServiceBar.vue'
 export default {
-  components: { ServiceBar },
-  name: 'index'
+  components: { ServiceBar, swiper, swiperSlide },
+  name: 'index',
+  data () {
+    return {
+      swiperOption: {
+
+      }
+    }
+  }
 }
 </script>
 
