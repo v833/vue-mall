498f5e233585463de4c80f42aed67021c0c808a1
v833 <2507301541@qq.com>
2021-06-05 17:26:23 +0800

    nav-menu

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -2,6 +2,36 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javescript:;">手机 电话卡</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">电视 盒子</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javescript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, index) in slideList" :key="index">
             <a :href="'/#/product/' + item.id"><img :src="item.img"></a>
@@ -78,12 +108,48 @@ export default {
 </script>
 
 <style lang="scss">
+  @import '../assets/scss/mixin';
   .index {
     .swiper-box {
+      position: relative;
       .swiper-container {
         height: 451px;
         img {
+          height: 100%;
           width: 100%;
+        }
+        .swiper-button-prev {
+          left: 274px
+        }
+      }
+      .nav-menu {
+        box-sizing: border-box;
+        width: 264px;
+        height: 451px;
+        background-color: #55585A7a;
+        position: absolute;
+        z-index: 10;
+        padding: 26px 0;
+        .menu-wrap {
+          .menu-item {
+            height: 50px;
+            line-height: 50px;
+            a {
+              display: block;
+              position: relative;
+              font-size: 16px;
+              color: white;
+              padding-left: 30px;
+              &:after {
+                content: ' ';
+                position: absolute;
+                right: 30px;
+                top: 17.5px;
+                @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+                vertical-align: middle;
+              }
+            }
+          }
         }
       }
     }
