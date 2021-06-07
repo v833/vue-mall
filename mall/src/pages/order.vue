2641b7827de06ea844ab13a522d8cfe6360b4c96
v833 <2507301541@qq.com>
2021-06-08 01:03:22 +0800

    订单组件父子结构

--- mall/src/pages/order.vue
+++ mall/src/pages/order.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
-    <order-header></order-header>
-    order
+    <order-header :title="title">
+      <template #describtion>
+        <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view></router-view>
-    <order-footer></order-footer>
+    <service-bar></service-bar>
+    <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
+import NavFooter from '../components/NavFooter.vue'
 import OrderHeader from '../components/OrderHeader'
-import OrderFooter from '../components/OrderFooter'
+import ServiceBar from '../components/ServiceBar'
 export default {
-  components: { OrderHeader, OrderFooter},
-  name: 'order'
+  components: { OrderHeader, ServiceBar, NavFooter},
+  name: 'order',
+  data () {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  mounted() {
+    let path = this.$route.path;
+    if (path == '/order/confirm') {
+      this.title = "订单确认"
+      this.tip = "请认真填写收获地址"
+    } else if (path == '/order/list') {
+      this.title = '订单列表'
+      this.tip = "请谨防钓鱼链接或诈骗电话"
+
+    } else if (path == '/order/pay') {
+      this.title = '订单支付'
+      this.tip = "请谨防钓鱼链接或诈骗电话"
+    }
+  },
 }
 </script>
 
--- mall/src/pages/orderConfirm.vue
+++ mall/src/pages/orderConfirm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    orderComfirm
+    confirm
   </div>
 </template>
 
