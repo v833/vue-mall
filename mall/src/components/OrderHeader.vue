efd97a13f938218f608e5807da770fe9e75bf3de
v833 <2507301541@qq.com>
2021-06-07 20:53:47 +0800

    cart

--- mall/src/components/OrderHeader.vue
+++ mall/src/components/OrderHeader.vue
@@ -1,15 +1,67 @@
 <template>
-  <div>
-    Header
+  <div class="order-header">
+    <div class="container clearfix">
+      <div class="header-logo">
+        <a href="/#/index"></a>
+      </div>
+      <div class="title">
+        <h2>{{title}}<slot name="describtion"></slot></h2>
+      </div>
+      <div class="username">
+        <a href="javascript:;">{{username}}</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'order-header'
+  name: 'order-header',
+  props: {
+    title: String
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    username () {
+      return this.$store.state.username
+    }
+  }
 }
 </script>
 
-<style lang="">
-  
+<style lang="scss" scoped>
+  .order-header {
+    padding: 30px 0;
+    .header-logo {
+      float: left;
+    }
+    .title, .username{
+      display: inline-block;
+      height: 55px;
+      line-height: 55px;
+    }
+    .title {
+      margin-left: 54px;
+      font-size: 28px;
+      float: left;
+      color: #333;
+      span {
+        font-size: 14px;
+        margin-left: 17px;
+        color: #999;
+        font-weight: bold;
+      }
+    }
+    .username {
+      float: right;
+      a {
+        color: #666;
+        font-size: 16px;
+      }
+    }
+  }
 </style>
